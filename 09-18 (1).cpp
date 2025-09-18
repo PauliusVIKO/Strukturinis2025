08bfc391f557789a8215ca87953408258873063b
PauliusVIKO <paulius.andrijauskas.it@gmail.com>
2025-09-18 09:35:22 +0300

    Create 09-18 (1).cpp

---
+++ 09-18 (1).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string vardas = "Paulius";
+    string pavarde = "Andrijauskas";
+    int amzius = 20;
+    string grupe = "PI25";
+    int kursas = 1;
+    string studijuprograma = "Programų sistemos";
+    
+    cout<<endl;
+    cout<<"Vardas: "<<vardas<<endl;
+    cout<<"Pavardė: "<<pavarde<<endl;
+    cout<<"Amžius: "<<amzius<<endl;
+    cout<<"Grupė: "<<grupe<<endl;
+    cout<<"Kursas: "<<kursas<<endl;
+    cout<<"Studijų programa: "<<studijuprograma<<endl;
+
+    return 0;
+}
