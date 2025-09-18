bf2b9c59f41c6c6b23ca9cddb1c099d2deeb8a7a
PauliusVIKO <paulius.andrijauskas.it@gmail.com>
2025-09-18 09:37:16 +0300

    Create 09-18(3).cpp

---
+++ 09-18(3).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string marke = "BMW";
+    string modelis = "M3 E30";
+    int metai = 1986;
+    string litrazas = "2.5 litražo";
+    string spalva = "juodos";
+    
+    cout<<"Automobilis "<<marke<<" "<<modelis<<" yra pagamintas "<<metai<<" metais. Jo motoras yra "<<litrazas<<". Automobilis yra "<<spalva<<" spalvos."<<endl;
+    
+    return 0;
+}
