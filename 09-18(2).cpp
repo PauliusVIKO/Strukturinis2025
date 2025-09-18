6e8222729699a37729791c97c602558b56066907
PauliusVIKO <paulius.andrijauskas.it@gmail.com>
2025-09-18 09:36:37 +0300

    Create 09-18(2).cpp

---
+++ 09-18(2).cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string pavadinimas = "Žalgiris Kaunas";
+    int ikurimas = 1944;
+    string savininkas = "UAB Žalgiris";
+    string arena = "Kauno Žalgirio arena";
+    int vietuskaicius = 17000;
+
+    cout<<"Pavadinimas: "<<pavadinimas<<endl;
+    cout<<"Įkūrimas: "<<ikurimas<<endl;
+    cout<<"Savininkas: "<<savininkas<<endl;
+    cout<<"Arena: "<<arena<<endl;
+    cout<<"Vietų skaičius: "<<vietuskaicius<<endl;
+
+    return 0;
+}
