93661d6162c45da930b16d966e7c5d1e1bb2680f
PauliusVIKO <paulius.andrijauskas.it@gmail.com>
2025-09-24 09:35:16 +0300

    Update 09-24(6).cpp

--- 09-24(6).cpp
+++ 09-24(6).cpp
@@ -6,7 +6,8 @@ int main() {
     int a = 1000;
     double b = 0.05;
     int c = 10;
-    int d = a*b*c;
+    int d = a+(a*b*c);
+    
     cout<<"Po 10 metų bus "<<d<<" Eur"<<endl;
     return 0;
 }
