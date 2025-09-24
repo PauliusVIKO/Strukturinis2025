a2497eaac87495334f5154b7a7c8c0dd1dd86057
PauliusVIKO <paulius.andrijauskas.it@gmail.com>
2025-09-24 08:47:57 +0300

    Create 09-24(1).cpp

---
+++ 09-24(1).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 9;
+    int b = 9;
+    int c = 4;
+    int d = 8;
+
+    double vidurkis = (a + b + c + d) / 4;
+
+    if (vidurkis >= 5) {
+        cout<<"Vidurkis teigiamas"<<endl;
+    }
+    if (vidurkis < 5) {
+        cout<<"Vidurkis neigiamas"<<endl;
+    }
+
+    return 0;
+}
