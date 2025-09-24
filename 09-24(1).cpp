eda034ead93b93a9ca29ce4de0de2c5c1ca66247
PauliusVIKO <paulius.andrijauskas.it@gmail.com>
2025-09-24 09:31:43 +0300

    Update 09-24(1).cpp

--- 09-24(1).cpp
+++ 09-24(1).cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
@@ -7,7 +8,7 @@ int main() {
     int c = 4;
     int d = 8;
 
-    double vidurkis = (a + b + c + d) / 4;
+    double vidurkis = (a+b+c+d)/4;
 
     if (vidurkis >= 5) {
         cout<<"Vidurkis teigiamas"<<endl;
