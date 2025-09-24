edb967ac1e053c8e3f218114c1505d0fd243cd5c
PauliusVIKO <paulius.andrijauskas.it@gmail.com>
2025-09-24 09:29:09 +0300

    Create 09-24(6).cpp

---
+++ 09-24(6).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int a = 1000;
+    double b = 0.05;
+    int c = 10;
+    int d = a*b*c;
+    cout<<"Po 10 metų bus "<<d<<" Eur"<<endl;
+    return 0;
+}
