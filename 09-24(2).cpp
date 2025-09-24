dc9d2d96a0d46ac8b6b578a164daea22eeabb74e
PauliusVIKO <paulius.andrijauskas.it@gmail.com>
2025-09-24 08:50:07 +0300

    Update 09-24(2).cpp

--- 09-24(2).cpp
+++ 09-24(2).cpp
@@ -7,7 +7,7 @@ int main() {
     int c = 4;
     int d = 8;
 
-    double vidurkis = (a + b + c + d) / 4;
+    double vidurkis = (a+b+c+d)/4;
 
     if (vidurkis >= 5) {
         cout<<"Vidurkis teigiamas"<<endl;
