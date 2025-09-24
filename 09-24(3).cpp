9ee64618711b1558ea06104172598ed8cfc54973
PauliusVIKO <paulius.andrijauskas.it@gmail.com>
2025-09-24 09:01:53 +0300

    Create 09-24(3).cpp

---
+++ 09-24(3).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 2;
+
+    if (a == 10) {
+        cout<<"puiku"<<endl;
+    } else if (a >= 9) {
+        cout<<"labai gerai"<<endl;
+    } else if (a >= 7) {
+        cout<<"gerai"<<endl;
+    } else if (a >= 5) {
+        cout<<"patenkinamai"<<endl;
+    } else if (a < 5) {
+        cout<<"egzaminas neišlaikytas"<<endl;
+    }
+
+    return 0;
+}
