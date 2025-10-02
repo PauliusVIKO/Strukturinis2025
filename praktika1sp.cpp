2b9b8722abd42648fc28480b2b09d3259054cab5
PauliusVIKO <paulius.andrijauskas.it@gmail.com>
2025-10-02 15:16:27 +0300

    praktika1sp.cpp

--- praktika1sp.cpp
+++ praktika1sp.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
     string valiuta;
     double suma;
     char buysell;
+    char test;
 
     double gbp = 0.8729;
     double usd = 1.1793;
@@ -16,39 +16,58 @@ int main() {
     double gbpsell = 0.9220;
     double usdsell = 1.2340;
     double inrsell = 107.8546;
-    
-    cout<<"Įveskite valiutą, kurią norite įsigyti (GBP, USD, INR): "<<endl;
-    cin>>valiuta;
-    
-    cout<<"Pirkti (B) ar parduoti (S)? "<<endl;
-    cin>>buysell;
-    
-    cout<<"Įveskite sumą: "<<endl;
-    cin>>suma;
-    
-    if (valiuta == "gbp" || valiuta == "GBP") {
-        if (buysell == 'b' || buysell == 'B') {
-            cout<<"Už "<<suma<<" € = "<<suma*gbpbuy<<" £."<<endl;
-        } else if (buysell  == 's' || buysell == 'S') {
-            cout<<"Už "<<suma<<" £ = "<<gbpsell/suma<<" €."<<endl;
+
+    do {
+        cout<<"Valiutų palyginimas:"<<endl;
+        cout<<"1 € = "<<gbp<<" £"<<endl;
+        cout<<"1 € = "<<usd<<" $"<<endl;
+        cout<<"1 € = "<<inr<<" ₹"<<endl;
+
+        cout<<"Įveskite sumą palyginimui: "<<endl;
+        cin>>suma;
+
+        cout<<suma<<" € = "<<suma*gbp<<" £"<<endl;
+        cout<<suma<<" € = "<<suma*usd<<" $"<<endl;
+        cout<<suma<<" € = "<<suma*inr<<" ₹"<<endl;
+
+        cout<<"Pirkti (B) ar parduoti (S)? "<<endl;
+        cin>>buysell;
+
+        cout<<"Įveskite valiutą, kurią norite įsigyti (GBP, USD, INR): "<<endl;
+        cin>>valiuta;
+
+        cout << "Įveskite sumą: "<<endl;
+        cin >> suma;
+
+        if (valiuta == "gbp" || valiuta == "GBP") {
+            if (buysell == 'b' || buysell == 'B') {
+                cout<<"Už " << suma << " € = "<<suma*gbpbuy <<" £."<<endl;
+            } else if (buysell == 's' || buysell == 'S') {
+                cout<<"Už "<<suma<<" £ = "<<gbpsell/suma<<" €."<<endl;
+            }
         }
-    }
-    
-    if (valiuta == "usd" || valiuta == "USD") {
-        if (buysell == 'b' || buysell == 'B') {
-            cout<<"Už "<<suma<<" € = "<<suma*usdbuy<<" $."<<endl;
-        } else if (buysell  == 's' || buysell == 'S') {
-            cout<<"Už "<<suma<<" $ = "<<usdsell/suma<<" €."<<endl;
+
+        if (valiuta == "usd" || valiuta == "USD") {
+            if (buysell == 'b' || buysell == 'B') {
+                cout<<"Už "<<suma<<" € = "<<suma*usdbuy<<" $."<<endl;
+            } else if (buysell == 's' || buysell == 'S') {
+                cout<<"Už "<<suma<<" $ = "<<usdsell/suma<< "€."<<endl;
+            }
         }
-    }
-    
-    if (valiuta == "inr" || valiuta == "INR") {
-        if (buysell == 'b' || buysell == 'B') {
-            cout<<"Už "<<suma<<" € = "<<suma*inrbuy<<" ₹."<<endl;
-        } else if (buysell  == 's' || buysell == 'S') {
-            cout<<"Už "<<suma<<" ₹ = "<<suma/inrsell<<" €."<<endl;
+
+        if (valiuta == "inr" || valiuta == "INR") {
+            if (buysell == 'b' || buysell == 'B') {
+                cout<<"Už "<<suma<<" € = "<<suma*inrbuy<<" ₹."<<endl;
+            } else if (buysell == 's' || buysell == 'S') {
+                cout<<"Už "<<suma<<" ₹ = "<<suma/inrsell<<" €."<<endl;
+            }
         }
-    }
-    
+
+        cout << "Ar norite tęsti? (T/N): "<<endl;
+        cin >> test;
+
+    } while (test == 'T' || test == 't');
+
+    cout << "Geros dienos!";
     return 0;
 }
